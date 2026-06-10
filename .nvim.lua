4ed6317185183d0fa035e6003495ea859a0e3cff
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-06-10 11:02:56 -0700

    feat(dev): cmd specific ignore

--- .nvim.lua
+++ .nvim.lua
@@ -1,3 +1,18 @@
-if default_fs_exclusion ~= nil then
-	table.insert(default_fs_exclusion, "*.spec.gen.nu")
+local ok, keymap = pcall(require, "lqr471814.lib.keymap")
+
+if ok then
+	local excludes = {
+		"*.spec.gen.nu"
+	}
+	if default_fs_exclusion ~= nil  then
+		for _, value in ipairs(default_fs_exclusion) do
+			table.insert(excludes, value)
+		end
+	end
+	keymap.overwrite_map("n", "<leader>ps", function()
+		Snacks.picker.grep({
+			hidden = true,
+			exclude = excludes,
+		})
+	end, "Find files via text content.")
 end
